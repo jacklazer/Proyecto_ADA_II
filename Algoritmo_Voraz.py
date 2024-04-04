99ab60dd20c7250c142b89a12c4d08a58e36f8d2
Juan CV <jacklazerjscv@gmail.com>
2024-04-04 10:28:32 -0500

    Cambios en Lector.py y Algoritmo_Voraz.py

--- Algoritmo_Voraz.py
+++ Algoritmo_Voraz.py
@@ -1,29 +1,40 @@
 import Algoritmo_Fuerza_Bruta
 
-def leer_archivo(nombre_archivo):
-    with open(nombre_archivo, 'r') as archivo:
-        lineas = archivo.readlines()
-    n = int(lineas[0].strip())
-    finca = []
-    for linea in lineas[1:]:
-        ts, tr, p = map(int, linea.strip().split(','))
-        finca.append((ts, tr, p))
-    return n, finca
 
+
+
+
+# Calculador de costos de ultimo riego
+# O(1)
 def calculador_dcdur(tablon, tiempo_total_de_regado):
     costo = tablon[2] * (tiempo_total_de_regado - tablon[0])
     return costo
 
+
+
+
+
+# Verificador de prioridad mas grande
+# O(1)
 def verificador_dpmg(a, b, finca):
     if (finca[a][2] >= finca[b][2]):
         return a
     elif (finca[b][2] > finca[a][2]):
         return b
 
+
+
+
+
+# Calculador de programacion optima
+# O(n+2 + (n+2-1) + ... + 2+2 + 1+2) = O(n) | n = len(costos_de_ultimo_riego)
 def calculador_dpo(costos_de_ultimo_riego, programacion_optima, finca, n):
+    # O(1)
     tablon_actual = 0
     costo_mas_costoso = 0
     tablon_mas_costoso = -1
+
+    # O(n) | n=len(costos_de_ultimo_riego)
     for costo_de_ultimo_riego in costos_de_ultimo_riego:
         if not(tablon_actual in programacion_optima):
             if (costo_de_ultimo_riego > costo_mas_costoso):
@@ -34,21 +45,40 @@ def calculador_dpo(costos_de_ultimo_riego, programacion_optima, finca, n):
                 costo_mas_costoso = costos_de_ultimo_riego[tablon_mas_costoso]
         tablon_actual += 1
     programacion_optima.append(tablon_mas_costoso)
+
+    # O(1)
     if (len(programacion_optima) == n):
         return programacion_optima
     else:
+        # O(n-1)
         return calculador_dpo(costos_de_ultimo_riego, programacion_optima, finca, n)
 
-def roV(nombre_archivo):
-    n, finca = leer_archivo(nombre_archivo)
+
+
+
+
+# Respuesta obtenida Voras
+# O(1 + n + n + 1 + n + n) = O(n)
+def roV(finca):
+    # O(1)
+    n = len(finca)
     tiempo_total_de_regado = 0
     costos_de_ultimo_riego = []
+
+    # O(n)
     for tablon in finca:
         tiempo_total_de_regado += tablon[1]
-    for tablon in finca:
-        costos_de_ultimo_riego.append(calculador_dcdur(tablon, tiempo_total_de_regado))
-    programacion_optima_ = []
-    programacion_optima = calculador_dpo(costos_de_ultimo_riego, programacion_optima_, finca, n)
-    return (programacion_optima, Algoritmo_Fuerza_Bruta.calculador_dctdr(Algoritmo_Fuerza_Bruta.calculador_didtdr(programacion_optima, finca), finca))
 
-print(roV("Prueba30.txt"))
+    # O(n)
+    for tablon in finca:
+        # O(1 + 1) = O(1)
+        costos_de_ultimo_riego.append(calculador_dcdur(tablon, tiempo_total_de_regado))
+
+    # O(1)
+    programacion_optima_ = []
+
+    # O(n)
+    programacion_optima = calculador_dpo(costos_de_ultimo_riego, programacion_optima_, finca, n)
+
+    # O(n + n) = O(2n) = O(n)
+    return (programacion_optima, Algoritmo_Fuerza_Bruta.calculador_dctdr(Algoritmo_Fuerza_Bruta.calculador_didtdr(programacion_optima, finca), finca))
--- Lector.py
+++ Lector.py
@@ -1,15 +1,16 @@
 import Algoritmo_Fuerza_Bruta
 import Algoritmo_Dinamico
+import Algoritmo_Voraz
 
 
 
 
 
 # archivos_txt_ordenados = ['PruebaX.txt']
-archivos_txt_ordenados = ['Prueba1.txt', 'Prueba2.txt', 'Prueba3.txt', 'Prueba4.txt', 'Prueba5.txt', 'Prueba6.txt', 'Prueba7.txt', 'Prueba8.txt', 'Prueba9.txt', 'Prueba10.txt']
-# archivos_txt_ordenados = ['Prueba1.txt', 'Prueba2.txt', 'Prueba3.txt', 'Prueba4.txt', 'Prueba5.txt', 'Prueba6.txt', 'Prueba7.txt', 'Prueba8.txt', 'Prueba9.txt', 'Prueba10.txt', 
-#                            'Prueba11.txt', 'Prueba12.txt', 'Prueba13.txt', 'Prueba14.txt', 'Prueba15.txt', 'Prueba16.txt', 'Prueba17.txt', 'Prueba18.txt', 'Prueba19.txt', 'Prueba20.txt', 
-#                            'Prueba21.txt', 'Prueba22.txt', 'Prueba23.txt', 'Prueba24.txt', 'Prueba25.txt', 'Prueba26.txt', 'Prueba27.txt', 'Prueba28.txt', 'Prueba29.txt', 'Prueba30.txt']
+# archivos_txt_ordenados = ['Prueba1.txt', 'Prueba2.txt', 'Prueba3.txt', 'Prueba4.txt', 'Prueba5.txt', 'Prueba6.txt', 'Prueba7.txt', 'Prueba8.txt', 'Prueba9.txt', 'Prueba10.txt']
+archivos_txt_ordenados = ['Prueba1.txt', 'Prueba2.txt', 'Prueba3.txt', 'Prueba4.txt', 'Prueba5.txt', 'Prueba6.txt', 'Prueba7.txt', 'Prueba8.txt', 'Prueba9.txt', 'Prueba10.txt', 
+                           'Prueba11.txt', 'Prueba12.txt', 'Prueba13.txt', 'Prueba14.txt', 'Prueba15.txt', 'Prueba16.txt', 'Prueba17.txt', 'Prueba18.txt', 'Prueba19.txt', 'Prueba20.txt', 
+                           'Prueba21.txt', 'Prueba22.txt', 'Prueba23.txt', 'Prueba24.txt', 'Prueba25.txt', 'Prueba26.txt', 'Prueba27.txt', 'Prueba28.txt', 'Prueba29.txt', 'Prueba30.txt']
 
 
 
@@ -18,10 +19,48 @@ archivos_txt_ordenados = ['Prueba1.txt', 'Prueba2.txt', 'Prueba3.txt', 'Prueba4.
 print("Digite\n1: Para usar fuerza bruta\n2: Para usar programacion dinamica\n3: Para usar programacion voraz")
 opcion = int(input("-> "))
 
-if (opcion == 1):
-    soluciones_str = ""
+print("Digite\n1: Para ejecutar varias pruebas \n2: Para ejecutar solo una prueba")
+opcion2 = int(input("-> "))
 
-    for nombre_archivo in archivos_txt_ordenados:
+if (opcion == 1):
+
+    if (opcion2 == 1):
+
+        print("Digite el numero de pruebas que requiere ejecutar (se empezaran a ejecutar las de menor numero de tablones):")
+        opcion3 = int(input("-> "))
+
+        soluciones_str = ""
+
+        for nombre_archivo in archivos_txt_ordenados[:opcion3]:
+            solucion_str = ""
+
+            with open(nombre_archivo, 'r') as archivo:
+                lineas = archivo.readlines()
+                finca = []
+                for linea in lineas[1:]:
+                    ts, tr, p = map(int, linea.strip().split(','))
+                    finca.append((ts, tr, p))
+
+                solucion = Algoritmo_Fuerza_Bruta.roFB(finca)
+
+                solucion_str += "Solucion de " + nombre_archivo + ":\n"
+
+                solucion_str += str(solucion[1]) + "\n"
+
+                for tablon in solucion[0]:
+                    solucion_str += str(tablon) + "\n"
+
+                print(solucion_str)
+                soluciones_str += solucion_str + "\n"
+
+        with open("Soluciones_Fuerza_Bruta.txt", "w") as archivo_:
+            archivo_.write(soluciones_str)
+
+    else:
+        print("Digite el numero de la prueba que requiere ejecutar:")
+        opcion3 = str(input("-> "))
+
+        nombre_archivo = "Prueba"+opcion3+".txt"
         solucion_str = ""
 
         with open(nombre_archivo, 'r') as archivo:
@@ -41,17 +80,52 @@ if (opcion == 1):
                 solucion_str += str(tablon) + "\n"
 
             print(solucion_str)
-            soluciones_str += solucion_str + "\n"
 
-    with open("Soluciones_Fuerza_Bruta.txt", "w") as archivo_:
-        archivo_.write(soluciones_str)
+        with open("Solucion_Fuerza_Bruta_"+nombre_archivo, "w") as archivo_:
+            archivo_.write(solucion_str)
+
 
 elif (opcion == 2):
-    soluciones_str = ""
 
-    for nombre_archivo in archivos_txt_ordenados:
-        solucion_str = ""
+    if (opcion2 == 1):
+
+        print("Digite el numero de pruebas que requiere ejecutar (se empezaran a ejecutar las de menor numero de tablones):")
+        opcion3 = int(input("-> "))
         
+        soluciones_str = ""
+
+        for nombre_archivo in archivos_txt_ordenados[:opcion3]:
+            solucion_str = ""
+
+            with open(nombre_archivo, 'r') as archivo:
+                lineas = archivo.readlines()
+                finca = []
+                for linea in lineas[1:]:
+                    ts, tr, p = map(int, linea.strip().split(','))
+                    finca.append((ts, tr, p))
+
+                solucion = Algoritmo_Dinamico.roPD(finca)
+
+                solucion_str += "Solucion de " + nombre_archivo + ":\n"
+
+                solucion_str += str(solucion[1]) + "\n"
+
+                for tablon in solucion[0]:
+                    solucion_str += str(tablon) + "\n"
+
+                print(solucion_str)
+                soluciones_str += solucion_str + "\n"
+
+        with open("Soluciones_Programacion_Dinamica.txt", "w") as archivo_:
+            archivo_.write(soluciones_str)
+
+    else:
+        print("Digite el numero de la prueba que requiere ejecutar:")
+        opcion3 = str(input("-> "))
+
+        nombre_archivo = "Prueba"+opcion3+".txt"
+        solucion_str = ""
+
         with open(nombre_archivo, 'r') as archivo:
             lineas = archivo.readlines()
             finca = []
@@ -69,10 +143,71 @@ elif (opcion == 2):
                 solucion_str += str(tablon) + "\n"
 
             print(solucion_str)
-            soluciones_str += solucion_str + "\n"
-                
-    with open("Soluciones_Programacion_Dinamica.txt", "w") as archivo_:
-        archivo_.write(soluciones_str)
+
+        with open("Solucion_Programacion_Dinamica_"+nombre_archivo, "w") as archivo_:
+            archivo_.write(solucion_str)
+
+
 else:
-    print("xd")
+
+    if (opcion2 == 1):
+
+        print("Digite el numero de pruebas que requiere ejecutar (se empezaran a ejecutar las de menor numero de tablones):")
+        opcion3 = int(input("-> "))
+        
+        soluciones_str = ""
+
+        for nombre_archivo in archivos_txt_ordenados[:opcion3]:
+            solucion_str = ""
+
+            with open(nombre_archivo, 'r') as archivo:
+                lineas = archivo.readlines()
+                finca = []
+                for linea in lineas[1:]:
+                    ts, tr, p = map(int, linea.strip().split(','))
+                    finca.append((ts, tr, p))
+
+                solucion = Algoritmo_Voraz.roV(finca)
+
+                solucion_str += "Solucion de " + nombre_archivo + ":\n"
+
+                solucion_str += str(solucion[1]) + "\n"
+
+                for tablon in solucion[0]:
+                    solucion_str += str(tablon) + "\n"
+
+                print(solucion_str)
+                soluciones_str += solucion_str + "\n"
+
+        with open("Soluciones_Programacion_Voraz.txt", "w") as archivo_:
+            archivo_.write(soluciones_str)
+
+    else:
+        print("Digite el numero de la prueba que requiere ejecutar:")
+        opcion3 = str(input("-> "))
+
+        nombre_archivo = "Prueba"+opcion3+".txt"
+        solucion_str = ""
+
+        with open(nombre_archivo, 'r') as archivo:
+            lineas = archivo.readlines()
+            finca = []
+            for linea in lineas[1:]:
+                ts, tr, p = map(int, linea.strip().split(','))
+                finca.append((ts, tr, p))
+
+            solucion = Algoritmo_Voraz.roV(finca)
+
+            solucion_str += "Solucion de " + nombre_archivo + ":\n"
+
+            solucion_str += str(solucion[1]) + "\n"
+
+            for tablon in solucion[0]:
+                solucion_str += str(tablon) + "\n"
+
+            print(solucion_str)
+
+        with open("Solucion_Programacion_Voraz_"+nombre_archivo, "w") as archivo_:
+            archivo_.write(solucion_str)
+
     
